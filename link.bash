795a11767037095e6d88aa146c2a99dea95883c1
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2013-06-08 15:13:32 -0400

    Fixing ghci warning when using .ghci

--- link.bash
+++ link.bash
@@ -2,7 +2,9 @@
 # creates links in the parent directory to all dotfiles in this directory.
 # HAS to be run in this directory
 
-FILES=".gitconfig .git_template .tmux.conf .vimrc .vundlebundles"
+FILES=".gitconfig .git_template .tmux.conf .vimrc .vundlebundles .ghci"
 
 for file in $FILES; do ln -s dotfiles/$file ../$file; done
 
+# Fix warning from ghci complaining it is writable
+chmod g-w .ghci
