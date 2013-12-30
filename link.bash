df8381c24314b2b2db232c14c5f59f171e6069a6
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2013-12-30 10:41:51 -0500

    Adding cabal config

--- link.bash
+++ link.bash
@@ -2,9 +2,12 @@
 # creates links in the parent directory to all dotfiles in this directory.
 # HAS to be run in this directory
 
-FILES=".gitconfig .git_template .tmux.conf .vimrc .ghci"
+LINKED_FILES=".gitconfig .git_template .tmux.conf .vimrc .ghci"
+COPIED_FILES=".cabal/config"
 
-for file in $FILES; do ln -s dotfiles/$file ../$file; done
+for file in $LINKED_FILES; do ln -s dotfiles/$file ../$file; done
+
+for file in $COPIED_FILES; do cp $file ../$file; done
 
 # Fix warning from ghci complaining it is writable
 chmod g-w .ghci
