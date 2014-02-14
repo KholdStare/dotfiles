bf4a2bc5ffa75586f427390edff61f91bda9dd70
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2014-02-14 00:49:09 -0500

    Greatly improving link.bash functionality when linking nested dirs

--- link.bash
+++ link.bash
@@ -2,12 +2,24 @@
 # creates links in the parent directory to all dotfiles in this directory.
 # HAS to be run in this directory
 
-LINKED_FILES=".bash_profile .gitconfig .git_template .tmux.conf .vimrc .ghci"
-COPIED_FILES=".cabal/config"
+# e.g.Turns ".cabal/config" into "../"
+function to_breadcrumbs() {
+    echo "$1" | sed -r -e 's@[^/]@@g' -e 's@/@../@g'
+}
 
-for file in $LINKED_FILES; do ln -s dotfiles/$file ../$file; done
+LINKED_FILES=".bash_profile .gitconfig .git_template .tmux.conf .vimrc .ghci .cabal/config bin/*"
+COPIED_FILES=""
 
-for file in $COPIED_FILES; do cp $file ../$file; done
+chmod u+x bin/*
+
+for file in $LINKED_FILES; do
+    mkdir -p ../$(dirname $file)
+    ln -s $(to_breadcrumbs $file)dotfiles/$file ../$file;
+done
+
+for file in $COPIED_FILES; do
+    cp $file ../$file;
+done
 
 # Fix warning from ghci complaining it is writable
 chmod g-w .ghci
